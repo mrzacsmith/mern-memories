df85706d58e6693fa11c990b75ce0591886bf84a
Zac Smith <zrs3141592@gmail.com>
2020-11-14 21:51:36 -0700

    scaffold app and set up mongo

---
+++ server/routes/posts.js
@@ -0,0 +1,33 @@
+const router = require('express').Router()
+
+const currentTime = new Date().toLocaleTimeString()
+
+// @desc			Test endpoint is working
+// @route 		GET /test
+router.get('/test', (req, res) => {
+  res.status(200).json({
+    message: `Endpoint is live at ${currentTime}`,
+  })
+})
+
+// @desc			Get all s
+// @route 		GET /
+router.get('/', (req, res) => {})
+
+// @desc			Get a  by id
+// @route 		GET /:id
+router.get('/:id', (req, res) => {})
+
+// @desc			Add a new
+// @route 		POST /
+router.post('/', (req, res) => {})
+
+// @desc			Update a  by id
+// @route 		PUT /:id
+router.put('/:id', (req, res) => {})
+
+// @desc			Remove a  by id
+// @route 		DELETE /:id
+router.delete('/:id', (req, res) => {})
+
+module.exports = router
--- server/server.js
+++ server/server.js
@@ -4,10 +4,16 @@ const morgan = require('morgan')
 const cors = require('cors')
 require('colors')
 
+const connectDB = require('./utils/connectDb.js')
+const postRouter = require('./routes/posts.js')
+
 const server = express()
 server.use(helmet())
 server.use(morgan('dev'))
 server.use(cors())
 server.use(express.json())
+connectDB()
+
+server.use('/api/posts', postRouter)
 
 module.exports = server
